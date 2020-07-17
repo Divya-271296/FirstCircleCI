662f06289411c7ee5e7a9f811db72dc81153c813
Divya-271296 <68419419+Divya-271296@users.noreply.github.com>
2020-07-17 17:36:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CircleCI_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!...This is my first CircleCi Project");
+        }
+    }
+}
